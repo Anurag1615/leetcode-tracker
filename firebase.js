9900911fab08a1011428516a99c201ec1c7ba7ed
Anurag1615 <69676929+Anurag1615@users.noreply.github.com>
2026-04-23 13:40:19 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -216,7 +216,7 @@ import { getFirestore, doc, setDoc, onSnapshot }
 // ── CONFIG ──────────────────────────────────────────────────────────────────
 // Replace these values with your own Firebase project credentials.
   const firebaseConfig = {
-    apiKey: "AIzaSyA4ihpCqfQZav2AqVX7-FOAaRt8Zai44X8",
+    apiKey: "AIzaSyCtkBhRhmznGe5kfKwpBAU6pNzjKWUZgP0",
     authDomain: "leetcode-pattern.firebaseapp.com",
     projectId: "leetcode-pattern",
     storageBucket: "leetcode-pattern.firebasestorage.app",
